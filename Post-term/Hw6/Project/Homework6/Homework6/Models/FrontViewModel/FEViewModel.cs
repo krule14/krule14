8b383ebb88d7cf2ce1d7d8dce3a518452b569935
krule14 <krule14@mail.wou.edu>
2019-01-07 00:23:44 -0800

    Shoot! Forgot to push... also forgot to do the video for hw 6 in a timely manner. Whoops. Anyway, Homeworks 6 and 7 are done. Gonna start working on 8 and 9 later.

---
+++ Post-term/Hw6/Project/Homework6/Homework6/Models/FrontViewModel/FEViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Homework6.Models.FrontViewModel
+{
+    public class FEViewModel
+    {
+        //Model from People 
+        public Person People { get; set; }
+
+        //Model from Customer
+        public Customer Customers { get; set; }
+
+        //Model from InvoiceLines
+        public List<InvoiceLine> InvoiceLines { get; set; }
+    }
+}
---
+++ Post-term/Hw7/Project/Homework7/Homework7/Controllers/RequestLogsController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Homework7.DAL;
+using Homework7.Models;
+
+namespace Homework7.Controllers
+{
+    public class RequestLogsController : Controller
+    {
+        private RequestLogContext db = new RequestLogContext();
+
+        // GET: RequestLogs
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ListOfQueries()
+        {
+            return View(db.RequestLogs.ToList());
+        }
+        public JsonResult Sticker(string userInput)
+        {
+
+            string apiKey = System.Web.Configuration.WebConfigurationManager.AppSettings["ApiKey"];
+            string apiURL = "https://api.giphy.com/v1/stickers/translate?api_key=" + apiKey + "&s=" + userInput;
+            Debug.WriteLine(apiURL);
+            Console.WriteLine(apiURL);
+
+            WebRequest apiRequest = WebRequest.Create(apiURL);
+            WebResponse getResponse = apiRequest.GetResponse();
+
+            Stream data = apiRequest.GetResponse().GetResponseStream();
+
+            string conStr = new StreamReader(data).ReadToEnd();
+
+
+
+            var serialize = new System.Web.Script.Serialization.JavaScriptSerializer();
+            var jsonObj = serialize.DeserializeObject(conStr);
+
+
+            var dbCon = db.RequestLogs.Create();//might be problem here with "RequestLogs" could be requestLog
+            dbCon.TimeNow = DateTime.Now;
+            dbCon.Request = userInput;
+            dbCon.IP = Request.UserHostAddress;
+            dbCon.BrowserType = Request.UserAgent;
+            db.RequestLogs.Add(dbCon);//Same problem?
+            db.SaveChanges();
+
+            data.Close();
+            getResponse.Close();
+
+            return Json(jsonObj, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
